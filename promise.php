bae5d52641dc9c9570fd5c721181048fc8d1549d
ariefkahfi <ariefkahfi@gmal.com>
2018-10-14 19:53:36 +0700

    add example and custom asyncRequest

--- promise.php
+++ promise.php
@@ -1,10 +1,12 @@
 <?php
 
 use Felis\Attica\AtticaPromise;
+use GuzzleHttp\Promise\Promise;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 
+// GET Async http client example
 $atticaPromise = new AtticaPromise();
 $wrongUrl = $atticaPromise->asyncGET("blasdaosd");
 
@@ -21,3 +23,36 @@ try {
 }catch(Exception $ex){
     var_dump($ex->getMessage());
 }
+
+// POST Async http client example (application/json)
+// chaining promises
+$m = $atticaPromise->asyncSendPOST("https://reqres.in/api/users",array(
+    'name'=>'hello',
+    'job'=>'world'
+),"application/json");
+
+$m->then(function($v) use ($atticaPromise){
+    echo $v . "\n";
+    return $atticaPromise->asyncSendPOST("http://httpbin.org/post",array(
+        'a'=>'aaa','b'=>'bbb'
+    ),'application/x-www-form-urlencoded')->wait();
+})->then(function($v) {
+    echo $v . "\n";
+});
+
+$m->wait();
+
+// POST Async with asyncRequest example
+$data = http_build_query(array(
+    'name'=>'asd',
+    'job'=>'dsa'
+));
+$headers = array(
+    'Content-type: application/x-www-form-urlencoded'
+);
+$custom = $atticaPromise->asyncRequest("http://httpbin.org/post",$headers , CURLOPT_POST , $data);
+$custom->then(function($v){
+    echo $v;
+});
+
+$custom->wait();
--- src/Felis/Attica/AtticaPromise.php
+++ src/Felis/Attica/AtticaPromise.php
@@ -10,9 +10,46 @@ use GuzzleHttp\Promise\Promise;
 
 class AtticaPromise {
 
-    public function asyncPOST($url) {
-        $promise = new Promise(function () use (&$promise , $url){
-            
+    public function asyncSendPOST($url , $data , $cType) {
+        $promise = new Promise(function () use (&$promise , $url , $data , $cType){
+            $atticaClient = AtticaClient::POST($url);
+            $atticaClient->send($data,$cType);
+            $atticaClient->execute();
+            $tError = $atticaClient->getErrCode();
+            if($tError != 0) {
+                $promise->reject($atticaClient->getErrMessage());
+                return;
+            }
+            $atticaResponse = $atticaClient->rawResponse();
+            $promise->resolve($atticaResponse);
+        });
+
+        return $promise;
+    }
+
+    public function asyncRequest($url , $httpHeaders , $httpMethod , $data) {
+        $promise = new Promise(function () use (&$promise , $url, $httpMethod, $httpHeaders, $data) {
+            $rCurl = curl_init($url);
+            curl_setopt($rCurl , $httpMethod , true);
+            curl_setopt($rCurl , CURLOPT_USERAGENT , "Attica-HTTPClient");
+            curl_setopt($rCurl , CURLOPT_RETURNTRANSFER , true);
+            if(isset($httpHeaders)){
+                curl_setopt($rCurl , CURLOPT_HTTPHEADER , $httpHeaders);
+            }
+
+            if($httpMethod == CURLOPT_POST) {
+                curl_setopt($rCurl , CURLOPT_POSTFIELDS , $data);
+            } 
+
+            $out = curl_exec($rCurl);
+
+            if(curl_errno($rCurl) != 0) {
+                $promise->reject(curl_error($rCurl));
+                curl_close($rCurl);
+                return;
+            }
+            $promise->resolve($out);
+            curl_close($rCurl);
         });
 
         return $promise;
